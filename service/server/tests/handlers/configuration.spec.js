04d08db88b7ef8d21831017f3d83c79377357c91
Josh Jarmain <jjarmain@gmail.com>
2016-09-18 21:39:47 -0700

    need to add git ignore

--- service/server/tests/handlers/configuration.spec.js
+++ service/server/tests/handlers/configuration.spec.js
@@ -1,10 +1,9 @@
-"use strict";
-const chai = require("chai");
-const sinon = require("sinon");
-const expect = chai.expect;
-const sinonChai = require("sinon-chai");
-chai.use(sinonChai);
-const configuration_1 = require("../../handlers/configuration");
-const configurationHandler = new configuration_1.default();
-var thing = sinon.createStubInstance(configurationHandler);
+// import * as chai from "chai";
+// import * as sinon from "sinon";
+// const expect = chai.expect;
+// const sinonChai = require("sinon-chai");
+// chai.use(sinonChai);
+// import ConfigurationHandler from "../../handlers/configuration"
+// const configurationHandler = new ConfigurationHandler();
+//  var thing = sinon.createStubInstance(configurationHandler);
 //# sourceMappingURL=configuration.spec.js.map
--- service/server/tests/handlers/configuration.spec.ts
+++ service/server/tests/handlers/configuration.spec.ts
@@ -1,9 +1,9 @@
-import * as chai from "chai";
-import * as sinon from "sinon";
-const expect = chai.expect;
-const sinonChai = require("sinon-chai");
-chai.use(sinonChai);
-import ConfigurationHandler from "../../handlers/configuration"
-const configurationHandler = new ConfigurationHandler();
- var thing = sinon.createStubInstance(configurationHandler);
+// import * as chai from "chai";
+// import * as sinon from "sinon";
+// const expect = chai.expect;
+// const sinonChai = require("sinon-chai");
+// chai.use(sinonChai);
+// import ConfigurationHandler from "../../handlers/configuration"
+// const configurationHandler = new ConfigurationHandler();
+//  var thing = sinon.createStubInstance(configurationHandler);
 
