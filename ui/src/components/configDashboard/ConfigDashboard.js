14e4e765fb1cf1e1b3370065983a08940d149645
Josh Jarmain <jjarmain@gmail.com>
2016-10-05 21:12:55 -0700

    clean up

--- ui/src/components/configDashboard/ConfigDashboard.js
+++ ui/src/components/configDashboard/ConfigDashboard.js
@@ -10,56 +10,43 @@ import Draft, {
 } from 'draft-js';
 var CodeUtils = require('draft-js-code');
 const PrismDraftDecorator = require('draft-js-prism')
-import Immutable from 'immutable';
-const {Map, List} = Immutable;
+// import Immutable from 'immutable';
+// const {Map, List} = Immutable;
 
-
-var FIRST_CODE = 'var message = "Hello World"\n    + "with four spaces indentation"\n\nconsole.log(message);';
-var SECOND_CODE = 'var message = "Hello World"\n  + "with 2 spaces indentation"\n\nconsole.log(message);';
-
+var emptyState = convertFromRaw({
+    entityMap:{},
+    blocks:[
+        {
+            type: 'code-block',
+            text: '{\n"key":"value"\n}'
+        },
+    ]
+})
 
 class ConfigDashboard extends React.Component {
     constructor(props) {
         super(props);
 
         var decorator = new PrismDraftDecorator();
-        var contentState = convertFromRaw({
-            entityMap: {},
-            blocks: [
-                {
-                    type: 'header-one',
-                    text: 'Demo for draft-js-code'
-                },
-                {
-                    type: 'unstyled',
-                    text: 'Type some JavaScript below, Use "Command+Return" (or "Ctrl+Return" on Windows) to split/exit a code blocks:'
-                },
-                {
-                    type: 'code-block',
-                    text: FIRST_CODE
-                },
-                {
-                    type: 'unstyled',
-                    text: 'And this is a code block with 2 spaces indentation'
-                },
-                {
-                    type: 'code-block',
-                    text: SECOND_CODE
-                }
-            ]
-        })
 
         this.state = {
-            editorState: EditorState.createWithContent(contentState, decorator),
+            editorState: EditorState.createWithContent(emptyState, decorator),
         };
 
         this.focus = () => this.refs.editor.focus();
-        this.onChange = (editorState) => this.setState({editorState});
+        this.onChange = (editorState) =>  {
+            var currentContent = editorState.getCurrentContent();
+            if (!currentContent.hasText()) {
+                const editorState = EditorState.push(this.state.editorState, emptyState);
+                this.setState({ editorState });  
+            }
+            else {
+                this.setState({editorState})
+            }
+        };
 
         this.handleKeyCommand = (command) => this._handleKeyCommand(command);
         this.keyBindingFn = (e) => this._keyBindingFn(e);
-        this.toggleBlockType = (type) => this._toggleBlockType(type);
-        this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
         this.onTab = (e) => this._onTab(e);
         this.onReturn = (e) => this._onReturn(e);
     }
@@ -97,24 +84,6 @@ class ConfigDashboard extends React.Component {
         return Draft.getDefaultKeyBinding(e);
     }
 
-    _toggleBlockType(blockType) {
-        this.onChange(
-            RichUtils.toggleBlockType(
-                this.state.editorState,
-                blockType
-            )
-        );
-    }
-
-    _toggleInlineStyle(inlineStyle) {
-        this.onChange(
-            RichUtils.toggleInlineStyle(
-                this.state.editorState,
-                inlineStyle
-            )
-        );
-    }
-
     _onTab(e) {
         let editorState = this.state.editorState;
 
@@ -141,37 +110,15 @@ class ConfigDashboard extends React.Component {
     }
 
     render() {
-        const {editorState} = this.state;
-
-        // If the user changes block type before entering any text, we can
-        // either style the placeholder or hide it. Let's just hide it now.
-        let className = 'RichEditor-editor';
-        var contentState = editorState.getCurrentContent();
-        if (!contentState.hasText()) {
-            if (contentState.getBlockMap().first().getType() !== 'unstyled') {
-                className += ' RichEditor-hidePlaceholder';
-            }
-        }
-
         return (
             <div className="RichEditor-root">
-                <BlockStyleControls
-                    editorState={editorState}
-                    onToggle={this.toggleBlockType}
-                />
-                <InlineStyleControls
-                    editorState={editorState}
-                    onToggle={this.toggleInlineStyle}
-                />
-                <div className={className} onClick={this.focus}>
+                <div className='RichEditor-editor' onClick={this.focus}>
                     <Editor
-                        blockStyleFn={getBlockStyle}
                         customStyleMap={styleMap}
-                        editorState={editorState}
+                        editorState={this.state.editorState}
                         handleKeyCommand={this.handleKeyCommand}
                         keyBindingFn={this.keyBindingFn}
                         onChange={this.onChange}
-                        placeholder="Tell a story..."
                         ref="editor"
                         spellCheck={true}
                         handleReturn={this.onReturn}
@@ -189,98 +136,7 @@ const styleMap = {
         backgroundColor: 'rgba(0, 0, 0, 0.05)',
         fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
         fontSize: 16,
-        padding: 2,
+        padding: 10
     },
 };
-
-function getBlockStyle(block) {
-    switch (block.getType()) {
-        case 'blockquote': return 'RichEditor-blockquote';
-        default: return null;
-    }
-}
-
-class StyleButton extends React.Component {
-    constructor() {
-        super();
-        this.onToggle = (e) => {
-            e.preventDefault();
-            this.props.onToggle(this.props.style);
-        };
-    }
-
-    render() {
-        let className = 'RichEditor-styleButton';
-        if (this.props.active) {
-            className += ' RichEditor-activeButton';
-        }
-
-        return (
-            <span className={className} onMouseDown={this.onToggle}>
-                {this.props.label}
-            </span>
-        );
-    }
-}
-
-const BLOCK_TYPES = [
-    {label: 'H1', style: 'header-one'},
-    {label: 'H2', style: 'header-two'},
-    {label: 'H3', style: 'header-three'},
-    {label: 'H4', style: 'header-four'},
-    {label: 'H5', style: 'header-five'},
-    {label: 'H6', style: 'header-six'},
-    {label: 'Blockquote', style: 'blockquote'},
-    {label: 'UL', style: 'unordered-list-item'},
-    {label: 'OL', style: 'ordered-list-item'},
-    {label: 'Code Block', style: 'code-block'},
-];
-
-const BlockStyleControls = (props) => {
-    const {editorState} = props;
-    const selection = editorState.getSelection();
-    const blockType = editorState
-        .getCurrentContent()
-        .getBlockForKey(selection.getStartKey())
-        .getType();
-
-    return (
-        <div className="RichEditor-controls">
-            {BLOCK_TYPES.map((type) =>
-                <StyleButton
-                    key={type.label}
-                    active={type.style === blockType}
-                    label={type.label}
-                    onToggle={props.onToggle}
-                    style={type.style}
-                />
-            )}
-        </div>
-    );
-};
-
-var INLINE_STYLES = [
-    {label: 'Bold', style: 'BOLD'},
-    {label: 'Italic', style: 'ITALIC'},
-    {label: 'Underline', style: 'UNDERLINE'},
-    {label: 'Monospace', style: 'CODE'},
-];
-
-const InlineStyleControls = (props) => {
-    var currentStyle = props.editorState.getCurrentInlineStyle();
-    return (
-        <div className="RichEditor-controls">
-            {INLINE_STYLES.map(type =>
-                <StyleButton
-                    key={type.label}
-                    active={currentStyle.has(type.style)}
-                    label={type.label}
-                    onToggle={props.onToggle}
-                    style={type.style}
-                />
-            )}
-        </div>
-    );
-};
-
 export default ConfigDashboard;
