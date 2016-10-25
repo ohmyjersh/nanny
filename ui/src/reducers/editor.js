c8cf9c86f887812c1df439b5e6b8e5e40e903c0b
jjarmain <jjarmain@gmail.com>
2016-10-25 16:22:24 -0700

    working on new reducer and adding test dir, immutable 4 life

--- ui/src/reducers/editor.js
+++ ui/src/reducers/editor.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from '../constants/actionTypes'
+import Immutable from 'immutable';
 
 function setNannyEditor(state, editor) {
     return Object.assign({}, state, {
@@ -7,18 +8,27 @@ function setNannyEditor(state, editor) {
 }
 
 
-export default function(state = { nannyEditor:{
-                                id:'',
-                                currentSelection:'',
-                                editorState:'',
-                                rawContent:'',
-                                textContent:'',
-                                isValid:'',
-                                title:'',
-                                type:''
-                                }}, action) {
-            switch(action.type) {
-                case ActionTypes.: 
-            }
 
+const initialState = (type = '') => {
+    return {
+        id: '',
+        currentSelection: '',
+        editorState: '',
+        rawContent: '',
+        textContent: '',
+        isValid: true,
+        title: '',
+        type: type
+    }
+};
+
+export default function (state = Immutable.fromJS(initialState), action) {
+    switch (action.type) {
+        case ActionTypes.setNannyEditor:
+            return setNannyEditor();
+        case ActionTypes.resetEditor():
+            return resetEditor();
+        default:
+            return state;
+    }
 }
