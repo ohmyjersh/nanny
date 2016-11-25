32917eba45e50eba7597d0acfbf5263b1cd821c7
Josh Jarmain <jjarmain@gmail.com>
2016-11-24 17:04:25 -0800

    reducer

---
+++ ui/src/reducers/apiKeys.js
@@ -0,0 +1,29 @@
+import * as ActionTypes from '../constants/actionTypes'
+
+function generatingApiKey(state, status) {
+    console.log(status);
+    return Object.assign({}, state, {generating:stats});
+}
+
+function setApiKey(state, apiKey) {
+        console.log(apiKey);
+    return Object.assign({}, state, {apiKey:apiKey});
+}
+
+function getApiKeys(state, keys) {    
+    console.log(status);
+    return Object.assign({}, state, {apiKeys:keys})
+}
+
+export default function reducer(state = {apiKeys:[],generating:false, apiKey:''}, action) {
+    switch(action.type) {
+        case ActionTypes.GENERATE_APIKEY_REQUEST:
+            return generatingApiKey(state, action.status);
+        case ActionTypes.GENERATE_APIKEY_RESPONSE:
+            return setApiKey(state,action.apiKey);
+        case ActionTypes.GET_APIKEY_REPSONSE:
+            return setApiKey(state,action.keys);
+        default:
+            return state;
+    }
+}
