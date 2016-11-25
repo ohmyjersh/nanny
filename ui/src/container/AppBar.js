c249cefe46237b2e2b039f0bf5262dd6aff75188
Josh Jarmain <jjarmain@gmail.com>
2016-11-24 21:54:22 -0800

    add appbar template

---
+++ ui/src/container/AppBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+export default class AppBar {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<div>AppBar</div>)
+    }
+}
